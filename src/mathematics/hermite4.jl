0a0684a6a9dc4949b1afacf3c43003c3cd41eb4c
annapaganetty <anna.paganetty@gmail.com>
2024-09-11 12:29:57 +0200

    Funktion für Basisfunktionen: konformes und nicht konformes Rechteckelement

--- src/mathematics/hermite4.jl
+++ src/mathematics/hermite4.jl
@@ -1,20 +1,42 @@
-
-V = [-1 1 1 -1; -1 -1 1 1]
-
-# Einheitsmatrix
-I12 = Matrix{Int}(I, 12, 12);
-
-P = mmonomials(2, 3, QHat , (p1, p2) -> p1 + p2 <= 4 && p1 * p2 <4,type = Int)
-N = vcat(
-    [
-        [
-            ValueAtLF(p),
-            PDerivativeAtLF(p, [1, 0]),
-            PDerivativeAtLF(p, [0, 1])
-        ]
-        for p in eachcol(V)
-    ]...
-)
-M = [n(p) for p in P, n in N]
-Minv = round.(simplify.(M \ I12);digits = 3)
-H4 = Minv*P
+function hermiteelement(K::String; conforming=true)
+    if K == "1D"
+        P = monomials([0,1,2,3], IHat)
+        N = vcat([[ValueAtLF(p), DerivativeAtLF(p)] for p = [-1,1]]...)
+        M = [n(p) for p in P, n in N]
+        H4 = inv(M) * P
+    elseif K == "2D"
+        if conforming
+            a = 16
+            P = mmonomials(2, 3, QHat  ,type = Int)
+            N = vcat(
+                [
+                    [
+                        ValueAtLF(p),
+                        PDerivativeAtLF(p, [1, 0]),
+                        PDerivativeAtLF(p, [0, 1]),
+                        PDerivativeAtLF(p, [1, 1])
+                    ]
+                    for p in eachcol(V)
+                ]...
+            )
+        else 
+            a = 12
+            P = mmonomials(2, 3, QHat , (p1, p2) -> p1 + p2 <= 4 && p1 * p2 <4,type = Int)
+            N = vcat(
+                [
+                    [
+                        ValueAtLF(p),
+                        PDerivativeAtLF(p, [1, 0]),
+                        PDerivativeAtLF(p, [0, 1])
+                    ]
+                    for p in eachcol(V)
+                ]...
+            )
+        end
+        Imatrix = Matrix{Int}(I, a, a)
+        M = [n(p) for p in P, n in N]
+        Minv = round.(simplify.(M \ Imatrix);digits = 3)
+        H4 = Minv*P
+    end
+    return H4
+end
