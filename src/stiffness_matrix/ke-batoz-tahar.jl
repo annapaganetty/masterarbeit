c01dbd93d9320589e45478c2adf9937f3ad0d338
annapaganetty <anna.paganetty@gmail.com>
2025-02-21 12:51:52 +0100

    Verbesserung Berechnung Ke nach Batoz und Tahar

--- src/stiffness_matrix/ke-batoz-tahar.jl
+++ src/stiffness_matrix/ke-batoz-tahar.jl
@@ -1,34 +1,58 @@
+using MMJMesh.Geometries
 # Gauss points for numerical integration
 const gaussWeights = ones(4)
 const gaussPoints = 1 / sqrt(3) * [[-1, -1], [1, -1], [1, 1], [-1, 1]]
 
 # Shape functions and gradients
 V = [ -1 1 1 -1; -1 -1 1 1]
-const Ni = lagrangeelement(V)
-const ∇N = [∂x(Ni[1]) ∂x(Ni[2]) ∂x(Ni[3]) ∂x(Ni[4]); ∂y(Ni[1]) ∂y(Ni[2]) ∂y(Ni[3]) ∂y(Ni[4])]
+const N = MappingFromComponents(lagrangeelement(V)...)
+# const ∇ξN = MMJMesh.Mathematics.TransposeMapping(jacobian(N))
+# const ∇ηN = MMJMesh.Mathematics.TransposeMapping(jacobian(N))
 
 # Element matrix
 function plateKe(p)
     function keFunc(e)
         D = p.E*p.h^3 / 12*(1-p.ν^2) * [1 p.ν 0; p.ν 1 0; 0 0 (1-p.ν)/2]
-
         Ke = zeros(12,12)
-        jF = jacobianMatrix(e)
+        jF = jacobian(parametrization(geometry(e)))
+        
+        Hx = MappingFromComponents(btpHx(e)...) # 12 Element Vektor mit Hx Funktionen 
+        Hy = MappingFromComponents(btpHy(e)...) # 12 Element Vektor mit Hy Funktionen 
+        ∇ξN = MMJMesh.Mathematics.TransposeMapping(jacobian(Hx))
+        ∇ηN = MMJMesh.Mathematics.TransposeMapping(jacobian(Hy))
 
         for (ξ, w) ∈ zip(gaussPoints, gaussWeights)
             # Jacobi matrix ausgewertet an der Stelle ξ
-            J = [jF[1,1](ξ) jF[1,2](ξ); jF[2,1](ξ) jF[2,2](ξ)] 
-            Jinv = inv(J)
-            # B-Matrix gemäß Gl. 13 aus Batoz und Tahar Paper
-            B =    [hcat([[sum([Jinv[1,1] * ∂x(btpHx(e)[i])(ξ) + Jinv[1,2] * ∂y(btpHx(e)[i])(ξ)])] for i = 1:12]...);
-                    hcat([[sum([Jinv[2,1] * ∂x(btpHy(e)[i])(ξ) + Jinv[2,2] * ∂y(btpHy(e)[i])(ξ)])] for i = 1:12]...);
-                    hcat([[sum([Jinv[1,1] * ∂x(btpHy(e)[i])(ξ) + Jinv[1,2] * ∂y(btpHy(e)[i])(ξ) + Jinv[2,1] * ∂x(btpHx(e)[i])(ξ) + Jinv[2,2] * ∂y(btpHx(e)[i])(ξ)])] for i = 1:12]...)]
+            J = jF(ξ)
+            ∇ₓN = (inv(J') * ∇ξN(ξ))
+            ∇yN = (inv(J') * ∇ηN(ξ)) 
+            B = [∇ₓN[1,:]', ∇yN[1,:]', ∇ₓN[1,:]' + ∇yN[1,:]']
             Ke += w * B' * D * B * det(J)
         end
         return Ke
     end
     return keFunc
 end
+
+ 
+#------------------------------------------------------------------------------------------
+# N = MappingFromComponents((lagrangeelement(V))...)
+# ->  ist eine Funktion, die einen Vektor mit den Ergebnissen von den (Lagrange-) Funktionen ausgibt 
+#     Bsp.:   N(1,2) gibt einen Vektor mit den Ergebnissen der Funktionen für x1 = 1 und x2 = 2 aus
+
+# jacobian(N) = 
+# ->  gibt 4 x 2 Matrix aus mit den Ableitungen der (Lagrange-) Funktionen 
+#     1. Spalte = Ableitungen nach x1
+#     2. Spalte = Ableitungen nach x2
+
+# MMJMesh.Mathematics.TransposeMapping(jacobian(NPaga))
+# ->  gibt transponierte Matrix von jacobian(NPaga) aus
+#     1. Spalte = Gradient Funktion 1
+#     2. Spalte = Gradient Funktion 2
+#     3. Spalte = Gradient Funktion 3
+#     4. Spalte = Gradient Funktion 4
+#------------------------------------------------------------------------------------------
+
 
 # Element vector
 function plateRe(q)
